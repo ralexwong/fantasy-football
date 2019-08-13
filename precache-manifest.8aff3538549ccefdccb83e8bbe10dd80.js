7780a36ee163337f34d3bdc95661d0b9c7ab3bc8
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-08-12 22:21:11 -0700

    Updates

---
+++ precache-manifest.8aff3538549ccefdccb83e8bbe10dd80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7df90fde050b25267c3338ca252d92e",
+    "url": "/fantasy-football/index.html"
+  },
+  {
+    "revision": "7600a56f2c0906c9bfc9",
+    "url": "/fantasy-football/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/fantasy-football/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7600a56f2c0906c9bfc9",
+    "url": "/fantasy-football/static/js/main.7e82dc9d.chunk.js"
+  },
+  {
+    "revision": "d117053fb72dccf7b2ba",
+    "url": "/fantasy-football/static/js/runtime~main.5861c355.js"
+  }
+]);
