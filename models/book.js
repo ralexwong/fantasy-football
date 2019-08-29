b2c9b20a1527e6a98293b576a3c51935e8e6d8ee
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-08-28 17:30:31 -0700

    added more completed

--- models/book.js
+++ models/book.js
@@ -3,9 +3,12 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  subtitle: { type: String },
+  authors: { type: [String], required: true },
+  link: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  googleId: { type: String, required: true, unique: true }
 });
 
 const Book = mongoose.model("Book", bookSchema);
