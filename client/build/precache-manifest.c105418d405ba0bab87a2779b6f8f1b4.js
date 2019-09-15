875ecaa18cd0983b07b1b33b5a3b0f80bd93e737
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-09-14 21:30:41 -0700

    test

---
+++ client/build/precache-manifest.c105418d405ba0bab87a2779b6f8f1b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9136af8b3ec019cc5918",
+    "url": "/static/js/main.5484b800.chunk.js"
+  },
+  {
+    "revision": "08acc7cb95fc84845294",
+    "url": "/static/js/2.f816f43c.chunk.js"
+  },
+  {
+    "revision": "9136af8b3ec019cc5918",
+    "url": "/static/css/main.b963e030.chunk.css"
+  },
+  {
+    "revision": "1a079d966b02f203b2cdb716aa28b2fc",
+    "url": "/index.html"
+  }
+];
